cf131a0bd7f65bb8e44681de3bfebba5e69bef02
dark-souls-slays <n3m0a3u@gmail.com>
2018-06-17 02:43:31 +0800

    preprocessing and initial PCA trials

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Unable to download data because the page was down. Had to manually extract pixel values.
+
+Dimensionality Reduction
+  Find an eigenvector for each matrix 28x28. Total Eigenvectors = Total Pictures
+  The dimensionality of each eigenvector is 28*28 = 784
--- char_recog.py
+++ char_recog.py
@@ -1,9 +1,19 @@
 from sklearn.datasets import fetch_mldata
 import numpy as np
+from numpy import linalg as LA
 import matplotlib.pyplot as plt
 import matplotlib.pylab as plb
 import struct
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+import sys
 
+"""
+The resulting images contain grey levels as a result of the anti-aliasing technique used by the normalization algorithm.
+The images were centered in a 28x28 image by computing the center of mass of the pixels, and translating the image so as
+to position this point at the center of the 28x28 field.
+
+"""
 
 
 def read_images(images_name):
@@ -31,11 +41,9 @@ def read_images(images_name):
                     pixel = struct.unpack('B',byte[0])[0]
                     image.append(pixel)
             ds_images.append(image)
-
     finally:
         f.close()
     return ds_images
-
 
 def read_labels(labels_name):
     #returns an array of labels
@@ -59,26 +67,70 @@ def read_labels(labels_name):
         f.close()
     return ds_labels
 
-
 def read_dataset(images_name,labels_name):
-    #reads an image-file and a labels file, and returns an array of tuples of
-    #(flattened_image, label)
+    #reads an image-file and a labels file, and returns an array of tuples of (flattened_image, label)
     images = read_images(images_name)
     labels = read_labels(labels_name)
     assert len(images) == len(labels)
     return zip(images,labels)
 
+def preprocess(image1D):
+    image1DNew =  np.array([])
+    image1DNew.astype(float)
+    for i in range(784):
+        result = float(image1D[i])/255.0
+        image1DNew = np.append(image1DNew, result)
+    return image1DNew
 
-testset = read_dataset("test_images","test_labels")
-trainingset = read_dataset("train_images","train_labels")
+def PCA(image1D):
+    #784 original image dimension
+    """
+    image = image1D.reshape(28,28)
+    Rx = np.corrcoef(image)
+    print(np.shape(Rx))
+    print(Rx)
+    w, v = LA.eig(np.diag((Rx)))
+    print("Eigenvalues Rx")
+    print(w)
+
+    #Find proper dimensionality
+    while (newD<11):
+        newD = newD + 1
+    """
+    sys.setrecursionlimit(10000)
+    pca = PCA(52)
+    pca.fit(image1D)
+    print(np.shape(pca.components_))
+        #ratio = sum(pca.explained_variance_) / sum(w)
+    """
+    # access values and vectors
+    print(pca.components_)
+    print(pca.explained_variance_)
+    # transform data
+    B = pca.transform(A)
+    print("Projection")
+    print(B)
+    """
+
+#testset = read_dataset("test_images","test_labels") #an array of 70000 labels (0~9)
+trainingset = read_dataset("train_images","train_labels") #a 70000x784 numpy array which contains all examples with each
 dataset = trainingset
 
 fig = plt.figure(figsize=(10,20))
-for i in range(50):
+
+for i in range(1):
     sp = fig.add_subplot(10,5,i+1)
     sp.set_title(dataset[i][1])
     plt.axis('off')
-    image = np.array(dataset[i][0]).reshape(28,28)
-    plt.imshow(image,interpolation='none',cmap=plb.gray(),label=dataset[i][1])
-
-plt.show()
+    image1D = np.array(dataset[i][0])#.reshape(28,28)#first 50 images
+    print("IMAGE IN 1D")
+    print(image1D)
+    image1DNew = preprocess(image1D)
+    print("IMAGE AFTER PREPROCESSING")
+    print(image1DNew)
+    #REDUCING DIMENSION
+    PCA(image1DNew)
+    image = image1D.reshape(28,28)#first 50 images
+    #print(image)
+    #plt.imshow(image,interpolation='none',cmap=plb.gray(),label=dataset[i][1])
+#plt.show()
